0e9f95b76fd1a66455637463418718474dedc686
César <cesar.cardinale@etu.univ-amu.fr>
2021-01-26 16:07:19 +0100

    Test affichage photoCard dans mainWindow

--- photocard.cpp
+++ photocard.cpp
@@ -9,12 +9,17 @@ photoCard::photoCard(QWidget *parent, Image *image) :
 {
     this->image = image;
     ui->setupUi(this);
+
+    QPalette pal = palette();
+    pal.setColor(QPalette::Background, Qt::white);
+    setAutoFillBackground(true);
+    setPalette(pal);
+
     QGraphicsDropShadowEffect *effect = new QGraphicsDropShadowEffect;
     effect->setBlurRadius(20);
     effect->setXOffset(0);
     effect->setYOffset(0);
     effect->setColor(Qt::black);
-
 
     ui->photo->setGraphicsEffect(effect);
 }
