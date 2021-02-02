a5a7e42c6f3dff6fd2a9312cb276abc7f98cd8b0
Baptiste Gasser <baptistegasser36@gmail.com>
2021-02-02 16:00:48 +0100

    Updated the Tag model
    - removed ID, the Value is now the unique ID
    - Added operator for comparison and debug

--- app/model/tag.h
+++ app/model/tag.h
@@ -1,18 +1,26 @@
 #ifndef TAG_H
 #define TAG_H
 
-#include "QString"
+#include <QDebug>
+#include <QString>
 
 class Tag
 {
 public:
     Tag() = default;
+    Tag(QString value): Tag(value, "#000000") {};
+    Tag(QString value, QString color): value(value), color(color) {};
 
-    int ID;
     QString value;
     QString color;
 
-    bool operator== (const Tag& tag) const { return this->value == tag.value; }
+    bool operator== (const Tag& tag) const { return value == tag.value && color == tag.color; }
+    bool operator!= (const Tag& tag) const { return !(*this == tag); }
+
+    operator QString() const {
+        QString s = QString("{ Value: %1, Color: %2 }").arg(value, color);
+        return s;
+    }
 };
 
 #endif // TAG_H
