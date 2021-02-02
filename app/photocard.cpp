42a4f0eb3772c12cbace2fde5158b1eae850dc15
César <cesar.cardinale@etu.univ-amu.fr>
2021-02-02 12:03:26 +0100

    Intégration des TagButton dans photoCard

--- app/photocard.cpp
+++ app/photocard.cpp
@@ -1,6 +1,8 @@
 #include "photocard.h"
 #include "ui_photocard.h"
+#include "tagbutton.h"
 
+#include <iostream>
 #include <QGraphicsDropShadowEffect>
 
 photoCard::photoCard(QWidget *parent, Image *image) :
@@ -9,6 +11,8 @@ photoCard::photoCard(QWidget *parent, Image *image) :
 {
     this->image = image;
     ui->setupUi(this);
+
+    //if (img.load(image->path)) ui->photo->setStyleSheet("background-image: url('"+image->path+"')");
 
     QPalette pal = palette();
     pal.setColor(QPalette::Background, Qt::white);
@@ -22,6 +26,36 @@ photoCard::photoCard(QWidget *parent, Image *image) :
     effect->setColor(Qt::black);
 
     ui->photo->setGraphicsEffect(effect);
+
+    TagButton * tagbutton = new TagButton(nullptr);
+    TagButton * tagbutton2 = new TagButton(nullptr);
+    TagButton * tagbutton3 = new TagButton(nullptr);
+
+    tagbutton->setMinimumWidth(70);
+    tagbutton2->setMinimumWidth(70);
+    tagbutton3->setMinimumWidth(70);
+
+    QHBoxLayout *layout = new QHBoxLayout(ui->desc_scroll);
+
+    /*
+    for(int i(0); i < image->feelingTags.size(); i++){
+        TagButton * tagbutton = new TagButton(nullptr);
+        tagbutton->setTag(image->feelingTags[i]);
+        layout->addWidget(tagbutton);
+    }
+    */
+
+    layout->addWidget(tagbutton);
+    layout->addWidget(tagbutton2);
+    layout->addWidget(tagbutton3);
+
+     QWidget *area = new QWidget(ui->desc_scroll);
+     area->setLayout(layout);
+     ui->desc_scroll->setWidget(area);
+     ui->desc_scroll->setWidgetResizable(true);
+
+     ui->desc_scroll->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+     ui->desc_scroll->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 }
 
 photoCard::~photoCard()
