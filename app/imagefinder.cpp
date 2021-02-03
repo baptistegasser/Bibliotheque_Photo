bca3fedfdd05ccbc3eb03988a274dacfd80f237a
benjamin-benesti <benjamin.ben-esti@etu.univ-amu.fr>
2021-02-03 01:40:21 +0100

    image finder terminé (manque filtre pour récupérer fichier photo uniquement )

--- app/imagefinder.cpp
+++ app/imagefinder.cpp
@@ -4,40 +4,8 @@
 #include "QFileDialog"
 #include "iostream"
 
-/*ImageFinder::ImageFinder(const QDir rootDir)
-{
-    this->rootDir = rootDir;
-}
+using namespace std;
 
-QFileInfoList ImageFinder::getImagesList() const
-{
-    QFileInfoList files = QFileInfoList();
-
-    QQueue<QDir> queue = QQueue<QDir>();
-    queue.enqueue(rootDir);
-
-    QDir currentDir;
-    QFileInfoList currentFilesInfos;
-    QDir::Filters filters = QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Readable | QDir::NoSymLinks;
-    while (!queue.empty()) {
-        currentDir = queue.dequeue();
-        currentFilesInfos = currentDir.entryInfoList(filters, QDir::DirsLast);
-
-        QFileInfo fileInfo;
-        QMutableListIterator<QFileInfo> it (currentFilesInfos);
-        while (it.hasNext()) {
-            fileInfo = it.next();
-
-            if (fileInfo.isDir()) {
-                queue.enqueue(fileInfo.absoluteDir());
-            } else {
-                files.append(fileInfo);
-            }
-        }
-    }
-
-    return files;
-}*/
 
 ImageFinder::ImageFinder()
 {
@@ -58,31 +26,25 @@ QFileInfoList ImageFinder::getImagesList() const
 
 
     QDir currentDir;
-    QList<QDir> visited;
     QFileInfoList currentFilesInfos;
     QDir::Filters filters = QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Readable | QDir::NoSymLinks;
     while (!queue.empty()) {
         currentDir = queue.dequeue();
-        visited.append(currentDir);
         currentFilesInfos = currentDir.entryInfoList(filters, QDir::DirsLast);
         for(const QFileInfo &fileInfo : qAsConst(currentFilesInfos))
         {
-            if (!visited.contains(fileInfo.absoluteDir()))
-            {
                 if (fileInfo.isDir()) {
-                    queue.enqueue(fileInfo.absoluteDir());
+                    queue.enqueue(fileInfo.absoluteFilePath());
                 } else {
                     files.append(fileInfo);
                 }
             }
-
         }
-    }
-    for(const QFileInfo &q : qAsConst(files))
-    {
-        std::cout << q.absolutePath().toStdString() << std::endl;
-    }
 
+    for (QFileInfo fic : files)
+    {
+        cout << fic.baseName().toStdString() << endl;
+    }
     return files;
 
 
