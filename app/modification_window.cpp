80cf0e3622b2b19509f902aaa74033c1843e5948
Ben <benjamin.benesti@gmail.com>
2021-02-12 19:19:32 +0100

    Recadrage propre

--- app/modification_window.cpp
+++ app/modification_window.cpp
@@ -46,16 +46,16 @@ void Modification_window::updateImage()
         _my_reset->show();
     }
     picture  = QPixmap(img.path);
+    if(img.cropped)
+    {
+        _my_recadrer->hide();
+        QRect rect (img.crop_x,img.crop_y,img.crop_width,img.crop_height);
+        picture = picture.copy(rect);
+    }
     if(img.resized)
     {
         picture = picture.scaled(img.res_width,img.res_height);
     }
-    if(img.cropped)
-    {
-        QRect rect (img.crop_x,img.crop_y,img.crop_width,img.crop_height);
-        picture = picture.copy(rect);
-    }
-
     _my_slider->setValue(1);
     if(img.resized)
     {
@@ -138,6 +138,7 @@ void Modification_window::backToOriginal()
 {
     img.resized = false;
     img.cropped = false;
+    _my_recadrer->show();
     DB::getImageDao().save(img);
     updateImage();
 }
@@ -321,11 +322,11 @@ void Modification_window::cropped ()
     Resizable_rubber_band * band = new Resizable_rubber_band(&_frame);
     if (img.resized)
     {
-         band->move((_frame.width()/2)-(img.res_width/2),(_frame.height()/2)-(img.res_height/2));
+        band->move((_frame.width()/2)-(img.res_width/2),(_frame.height()/2)-(img.res_height/2));
     }
     else if (img.cropped)
     {
-         band->move((_frame.width()/2)-(img.crop_width/2),(_frame.height()/2)-(img.crop_height/2));
+        band->move((_frame.width()/2)-(img.crop_width/2),(_frame.height()/2)-(img.crop_height/2));
     }
     else
     {
@@ -358,14 +359,18 @@ void Modification_window::cropped ()
     _my_slider->setEnabled(false);
     _my_return_button->setEnabled(false);
 
-
-    croppAccept = new QPushButton("Redimensionner");
-    croppAccept->setStyleSheet("background-color:white;border:1px solid black;");
+    croppAccept = new QPushButton("Recadrer");
+    croppAccept->setFixedSize(200,50);
+    croppAccept->setStyleSheet("background-color:white;color:black ;border:solid darkgrey ;border-width: 1px 1px 1px 1px;border-radius:10px;");
     croppCancel = new QPushButton("Annuler");
-    zoom_layout->addWidget(croppAccept);
-    zoom_layout->addWidget(croppCancel);
+    croppCancel->setStyleSheet("background-color:white;color:black ;border:solid darkgrey ;border-width: 1px 1px 1px 1px;border-radius:10px;");
+    croppCancel->setFixedSize(200,50);
+    _cropp_layout->setAlignment(Qt::AlignHCenter);
+    _cropp_layout->addWidget(croppAccept);
+    _cropp_layout->addWidget(croppCancel);
 
-    croppAccept->setStyleSheet("background-color:grey;border:solid black 1px;");
+
+
     croppAccept->show();
     connect(croppAccept,&QPushButton::clicked,this,[=](){band->hide();this->toCrop(band->frameGeometry());});
     connect(croppCancel,&QPushButton::clicked,this,[=](){band->hide();this->cropCancel();});
@@ -374,11 +379,14 @@ void Modification_window::cropped ()
 
 void Modification_window::toCrop(QRect rect)
 {
+
     img.cropped = true;
     img.crop_x = rect.x();
     img.crop_y = rect.y();
     img.crop_width = rect.width();
     img.crop_height = rect.height();
+
+
 
     _my_detail_box->setEnabled(true);
     groupBox->setEnabled(true);
