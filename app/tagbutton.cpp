427bd24d937ff38a04e56edf38e4ed98612833d7
Ben <benjamin.benesti@gmail.com>
2021-02-12 23:14:22 +0100

    style tagbutton

--- app/tagbutton.cpp
+++ app/tagbutton.cpp
@@ -1,21 +1,11 @@
 #include "tagbutton.h"
 
 
-TagButton::TagButton(QWidget *parent)
-{
-    this->setText("Test");
-    this->setFixedWidth(this->sizeHint().width());
-    this->setFixedHeight(this->sizeHint().height());
-    this->setText("Test");
-     this->setStyleSheet("background-color:white;color:red ;border:solid red ;border-width: 1px 1px 1px 1px;border-radius:10px;");
-}
-
 TagButton::TagButton(QWidget *parent,QString name,QString color):QPushButton(parent)
 {
     this->setText(name);
-    this->setFixedWidth(this->sizeHint().width());
-    this->setFixedHeight(this->sizeHint().height());
-    this->setStyleSheet("background-color:white;color:"+color+";border:solid "+color+";border-width: 1px 1px 1px 1px;border-radius:10px;");
+    this->setFixedHeight(this->sizeHint().height()+5);
+    this->setStyleSheet("background-color:white;color:rgb(120,120,120);border:solid "+color+";border-width: 2px 2px 2px 2px;border-radius:10px;font-size:14px;padding-left:5px;padding-right:5px;");
 }
 
 
